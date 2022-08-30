7686a6dd4c85b93c6b8991846dbf0b71df4eea56
Jago Van Dam <Jago.VanDam@sa.gov.au>
2022-08-30 09:52:09 +0930

    updated chapter 2 in sidenav.html

--- HTML/scripts/load-sidenav.js
+++ HTML/scripts/load-sidenav.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){   
-    $("#mySidenav").load("https://cdsircsa.github.io/CDSIRC-Annual-Report-2021-22/HTML/sidenav.html");
+    //$("#mySidenav").load("https://cdsircsa.github.io/CDSIRC-Annual-Report-2021-22/HTML/sidenav.html"); //local
+    $("#mySidenav").load("sidenav.html"); //server
 });

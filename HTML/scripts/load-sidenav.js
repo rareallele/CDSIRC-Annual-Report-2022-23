605389c2c9609a1a5847ec8e4a9e9e56082d9a12
Jago Van Dam <Jago.VanDam@sa.gov.au>
2022-10-26 10:01:20 +1030

    final changes. added Figures.html, updated sidenav

--- HTML/scripts/load-sidenav.js
+++ HTML/scripts/load-sidenav.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){   
-    $("#mySidenav").load("https://cdsircsa.github.io/CDSIRC-Annual-Report-2021-22/HTML/sidenav.html"); //local
-    //$("#mySidenav").load("sidenav.html"); //server
+    //$("#mySidenav").load("https://cdsircsa.github.io/CDSIRC-Annual-Report-2021-22/HTML/sidenav.html"); //local
+    $("#mySidenav").load("sidenav.html"); //server
 });

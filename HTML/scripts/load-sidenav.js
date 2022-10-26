e26fd03e03022cc4b7013f637e6b5a5e383fa78d
Jago Van Dam <Jago.VanDam@sa.gov.au>
2022-10-26 10:31:20 +1030

    minor fixes

--- HTML/scripts/load-sidenav.js
+++ HTML/scripts/load-sidenav.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){   
-    //$("#mySidenav").load("https://cdsircsa.github.io/CDSIRC-Annual-Report-2021-22/HTML/sidenav.html"); //local
     $("#mySidenav").load("sidenav.html"); //server
 });
